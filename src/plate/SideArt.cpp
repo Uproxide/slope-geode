4542418a0d98345ef3a28a837a24581f9cf410dd
acaruso-xx <acaruso@tuta.io>
2024-03-09 13:23:29 -0500

    🩹 Fix side art positioning

--- src/plate/SideArt.cpp
+++ src/plate/SideArt.cpp
@@ -24,32 +24,37 @@ bool SideArt::init(char const* image) {
     auto* director = cocos2d::CCDirector::sharedDirector();
     auto winSize = director->getWinSize();
 
+    float top = director->getScreenTop();
+    float bottom = director->getScreenBottom();
+    float left = director->getScreenLeft();
+    float right = director->getScreenRight();
+
     setContentSize(winSize);
     ignoreAnchorPointForPosition(true);
     setCascadeColorEnabled(true);
 
     auto* dl = Toolbox::createSprite(image);
     dl->setAnchorPoint({ 0.0f, 0.0f });
-    dl->setPosition({ 0.0f, 0.0f });
+    dl->setPosition({ -1.0f, -1.0f });
     addChild(dl);
 
     auto* dr = Toolbox::createSprite(image);
     dr->setAnchorPoint({ 1.0f, 0.0f });
     dr->setFlipX(true);
-    dr->setPosition({ winSize.width, 0.0f });
+    dr->setPosition({ winSize.width + 1.0f, -1.0f });
     addChild(dr);
 
     auto* ul = Toolbox::createSprite(image);
     ul->setAnchorPoint({ 0.0f, 1.0f });
     ul->setFlipY(true);
-    ul->setPosition({ 0.0f, winSize.height });
+    ul->setPosition({ -1.0f, winSize.height + 1.0f });
     addChild(ul);
 
     auto* ur = Toolbox::createSprite(image);
     ur->setAnchorPoint({ 1.0f, 1.0f });
     ur->setFlipX(true);
     ur->setFlipY(true);
-    ur->setPosition(winSize);
+    ur->setPosition({ winSize.width + 1.0f, winSize.height + 1.0f });
     addChild(ur);
 
     return true;
