66ee7f801a978268aea09066fb8ae0be8f1b3d42
acaruso-xx <acaruso@tuta.io>
2024-03-09 13:49:50 -0500

    ✨ PlayerNode uses player icon

--- src/slope/PlayerNode.cpp
+++ src/slope/PlayerNode.cpp
@@ -1,6 +1,8 @@
 #include "PlayerNode.hpp"
 
 #include <Geode/binding/SimplePlayer.hpp>
+#include <Geode/binding/GameManager.hpp>
+#include <Geode/loader/Log.hpp>
 
 namespace slope {
 
@@ -21,10 +23,29 @@ bool PlayerNode::init() {
         return false;
     }
 
+    auto* gameMgr = GameManager::sharedState();
+
+    int frame = gameMgr->getPlayerFrame();
+    auto color = gameMgr->colorForIdx(gameMgr->getPlayerColor());
+    auto color2 = gameMgr->colorForIdx(gameMgr->getPlayerColor2()); 
+    auto glowColor = gameMgr->colorForIdx(gameMgr->getPlayerGlowColor());
+
     setContentSize({ 30.0f, 30.0f });
 
-    auto* inner = SimplePlayer::create(2);
+    auto* inner = SimplePlayer::create(frame);
     inner->setPosition(getContentSize() / 2.0f);
+
+    inner->setColor(color);
+    inner->setSecondColor(color2);
+
+    if (gameMgr->getPlayerGlow()) {
+        inner->enableCustomGlowColor(glowColor);
+        inner->m_hasGlowOutline = true;
+    } else {
+        inner->disableCustomGlowColor();
+    }
+
+    inner->updateColors();
     addChild(inner);
 
     setAnchorPoint({ 0.5f, 0.5f });
